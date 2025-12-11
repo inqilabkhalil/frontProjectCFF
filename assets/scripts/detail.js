fc3cee128ed1622711f18b7a65a089f2318b63f1
Inqılab <inqlab.xelilov91@gmail.com>
2025-12-11 16:45:54 +0400

    endtask

--- assets/scripts/detail.js
+++ assets/scripts/detail.js
@@ -1,13 +1,26 @@
-const urlParams = new URLSearchParams(window.location.search)
-const id = urlParams.get("id")
-const data = `https://api.tvmaze.com/shows/${id}`;
-fetch(data).then(res=>res.json()).then(data=>{
-    console.log(data)
-    let container = document.querySelector(".container")
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+const dataUrl = `https://api.tvmaze.com/shows/${id}`;
+
+fetch(dataUrl)
+  .then(res => res.json())
+  .then(data => {
+    let container = document.querySelector(".container");
     container.innerHTML = `
-        <h1>${data.name}</h1>
-        <img src="${data.image?.original}" width="400px" height="500px">
-        <p>Rating: ${data.rating.average}</p>
-        <p>${data.summary}</p>
-      `;
-})
+      <h1>${data.name}</h1>
+
+      <img src="${data.image?.original}" width="400px" height="500px">
+
+      <p><strong>Rating:</strong> ${data.rating?.average || "No rating"}</p>
+
+      <p><strong>Language:</strong> ${data.language}</p>
+
+      <p><strong>Genres:</strong> ${data.genres.join(", ")}</p>
+
+      <p><strong>Status:</strong> ${data.status}</p>
+
+      <p><strong>Premiered:</strong> ${data.premiered}</p>
+
+    `;
+  });
